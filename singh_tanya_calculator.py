8c93e30789c76343ec977b017934a6fef2a3bcd1
Ms-Tanya-Singh <roz.tns@gmail.com>
2025-03-29 17:34:43 -0400

    Add files via upload

---
+++ singh_tanya_calculator.py
@@ -0,0 +1,50 @@
+#define functions for calculator values
+def add(a,b):
+    return a+b
+
+def subtract(a,b):
+    return a-b
+
+def multiply(a,b):
+    return a*b
+
+def divide(a,b):
+    if b == 0:
+        raise ValueError("Cannot divide by zero")
+    return a/b        
+
+def modulus(a,b):
+    if b == 0:
+        raise ValueError("Cannot perform modulus by zero")
+    return(a%b)
+
+def power(a,b):
+    return a**b
+
+def floor_divide(a,b):
+    if b == 0:
+        raise ValueError("cammot do floor division by zero")
+    return a // b
+
+def convert(value):
+    if '.' in value:
+        return float(value)
+    else: 
+        return int(value)
+# converting strings to int or float 
+
+#create dictionary of string to int or float
+
+operators = {
+'+' : add,
+'-' : subtract,
+'*' : multiply,
+'/' : divide,
+'%' : modulus,
+'**': power,
+'//': floor_divide
+}
+
+op = '//'
+opres = operators[op](3,4)
+print(opres) 
